8f5f369faa584f582d9135a29afa9c6e7706d9d6
Hasna <hassna.ahmad98@gmail.com>
2024-09-15 14:44:26 +0300

    border

--- main.js
+++ main.js
@@ -12,16 +12,24 @@ fetch(url)
     let box = document.createElement("div")
     container.appendChild(box)
     box.style.width = "40vh"
-    box.style.height = "50vh"
-    // box.style.border = "1px solid"
-    box.style.boxShadow = "1px 2px 3px gray";
+    box.style.height = "40vh"
+    box.style.border = "1px solid"
+    box.style.borderRadius = "25px"
+    box.style.boxShadow = "1px 5px 10px gray";
 
     let titlebox = document.createElement("h1")
     box.appendChild(titlebox)
     titlebox.textContent = item.name
+
+    let imgdiv = document.createElement("div")
+    box.appendChild(imgdiv)
+
     let imagebox = document.createElement("img")
     box.appendChild(imagebox)
     imagebox.src = item.image
+
+    imagebox.style.height = "20vh"
+    imagebox.style.width = "25vh"
    } )
 
 
